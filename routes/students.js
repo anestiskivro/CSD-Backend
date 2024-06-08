91b10308a4ab29e7952f51db0412f4bb0883d60e
anestiskivro <anestiskivro@gmail.com>
2024-06-08 14:10:20 +0300

    heroku origin

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require("../models")
+const cors = require('cors');
 const nodemailer = require('nodemailer');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
