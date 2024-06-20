2cc3cd2ebc76709c9ffaf91e9df4740f8d9aaa85
anestiskivro <anestiskivro@gmail.com>
2024-06-20 10:44:40 +0300

    email unique

--- models/Teachers.js
+++ models/Teachers.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
             validate: {
                 notEmpty: true
             }
--- models/students.js
+++ models/students.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
             validate: {
                 notEmpty: true
             }
